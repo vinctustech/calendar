f3a3eee87200181687aa052a918fe8e3a8c3375f
Edward A Maxedon <edadma@gmail.com>
2025-09-16 08:06:22 -0400

    remove unneeded exports

--- packages/library/src/index.ts
+++ packages/library/src/index.ts
@@ -4,10 +4,6 @@ export type { MonthCalendarProps } from './MonthCalendar'
 export { WeekCalendar } from './WeekCalendar'
 export type { WeekCalendarProps } from './WeekCalendar'
 
-// Backwards compatibility - alias MonthCalendar as Calendar
-export { MonthCalendar as Calendar } from './MonthCalendar'
-export type { MonthCalendarProps as CalendarProps } from './MonthCalendar'
-
 // Shared exports
 export type { CalendarEvent, CalendarLocale, BaseCalendarProps } from './shared/types'
 export { en, fr } from './shared/locales'
